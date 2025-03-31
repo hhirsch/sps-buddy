45038341f575e57ee888678d93af4724a9fe83d3
Henry Hirsch <2451426+hhirsch@users.noreply.github.com>
2025-03-31 15:29:04 +0200

    feat: add testing

--- main_test.go
+++ main_test.go
@@ -4,7 +4,12 @@ import (
 	"testing"
 )
 
+func resetState() {
+	parserIsInsideVariableBlock = false
+}
+
 func TestDetectsWhenInVariableBlock(test *testing.T) {
+	test.Cleanup(resetState)
 	handleLine("errorActive1 : Bool;")
 	if parserIsInsideVariableBlock {
 		test.Error("mixed camel case must be valid")
@@ -12,6 +17,7 @@ func TestDetectsWhenInVariableBlock(test *testing.T) {
 }
 
 func TestMixedCamelCaseVariableIsValid(test *testing.T) {
+	test.Cleanup(resetState)
 	parserIsInsideVariableBlock = true
 	handleLine("errorActive1 : Bool;")
 	if isErrorDetected {
@@ -20,9 +26,21 @@ func TestMixedCamelCaseVariableIsValid(test *testing.T) {
 }
 
 func TestMixedCamelCaseFunctionIsValid(test *testing.T) {
+	test.Cleanup(resetState)
 	parserIsInsideVariableBlock = true
 	handleLine("almError {InstructionName := 'Program_Alarm'; LibVersion := '1.0'} : Program_Alarm;")
 	if isErrorDetected {
 		test.Error("mixed camel case must be valid")
+	}
+}
+
+func TestVarInputTriggersInsideVariableBlock(test *testing.T) {
+	test.Cleanup(resetState)
+	if parserIsInsideVariableBlock {
+		test.Error("initial value of inside variable block should be false")
+	}
+	handleLine("VAR_INPUT")
+	if !parserIsInsideVariableBlock {
+		test.Error("did not detect that VAR_INPUT started")
 	}
 }
